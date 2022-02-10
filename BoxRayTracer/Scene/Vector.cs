9bb3053112c65a41831cf0481ffcb7a167821b86
Cameron Richards <crichards1717@gmail.com>
2022-02-09 17:01:32 -0800

    Added Cross() and Unit() to Vector, refactored Camera fields, implemented RayForPixel()

--- BoxRayTracer/Scene/Vector.cs
+++ BoxRayTracer/Scene/Vector.cs
@@ -14,9 +14,36 @@
 
         }
 
+        /// <summary>
+        /// Returns the length scalar of this vector
+        /// </summary>
+        /// <returns></returns>
         public double Length()
         {
             return Math.Sqrt(x * x + y * y + z * z);
+        }
+        
+        /// <summary>
+        /// Returns the cross product between this Vector
+        /// and given Vector, "b"
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public Vector Cross(Vector b)
+        {
+            double cX = this.y * b.z - this.z * b.y;
+            double cY = this.z * b.x - this.x * b.z;
+            double cZ = this.x * b.y - this.y * b.x;
+            return new Vector(cX, cY, cZ);
+        }
+
+        /// <summary>
+        /// Returns the unit vector equivalent for this Vector
+        /// </summary>
+        /// <returns></returns>
+        public Vector Unit()
+        {
+            return new Vector(this.x / this.Length(), this.y / this.Length(), this.z / this.Length());
         }
 
         #region operators
