7172e5455a5cc7b443ea4d822c6d56dc45197ae6
Cameron Richards <crichards1717@gmail.com>
2022-02-13 14:15:52 -0800

    Implemented BitMapRaytracer, built out WPF interface scaffold, rendered initial Sphere scene

---
+++ BoxRayTracer/BoxRayTracer/BitmapRaytracer.cs
@@ -0,0 +1,52 @@
+﻿using Scene;
+using System.Drawing;
+
+namespace BoxRayTracer
+{
+    internal class BitmapRaytracer
+    {
+        private readonly IDistanceEstimator dE;
+        private readonly Camera camera;
+        private readonly int dWidth;
+        private readonly int dHeight;
+        private readonly double maxDist;
+
+        public BitmapRaytracer(IDistanceEstimator dE, double maxDist, int dWidth, int dHeight, int fov, Vector camPos, Vector lookAt)
+        {
+            this.dE = dE;
+            this.dWidth = dWidth;
+            this.dHeight = dHeight;
+            this.maxDist = maxDist;
+            this.camera = new Camera(camPos, lookAt, fov, 1, (double)dHeight / dWidth, dWidth, dHeight);
+        }
+
+        public Bitmap Render()
+        {
+            Bitmap image = new(dWidth, dHeight);
+            for (int i = 0; i < dWidth; i++)
+            {
+                for (int j = 0; j < dHeight; j++)
+                {
+                    image.SetPixel(i, j, RayMarch((uint)i, (uint)j));
+                }
+            }
+            return image;
+        }
+
+        private Color RayMarch(uint x, uint y)
+        {
+            camera.RayForPixel(x, y, out Vector pos, out Vector rayDir);
+            double currentDist = dE.DE(pos);
+            while (!Utilities.IsEqualApprox(currentDist, 0))
+            {
+                pos += rayDir * currentDist;
+                currentDist = dE.DE(pos);
+                if (currentDist > maxDist)
+                {
+                    return Color.Black;
+                }
+            }
+            return Color.Purple;
+        }
+    }
+}
--- BoxRayTracer/BoxRayTracer/MainWindow.xaml.cs
+++ BoxRayTracer/BoxRayTracer/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
+using Scene;
+using Vector = Scene.Vector;
 
 namespace BoxRayTracer
 {
@@ -20,9 +12,35 @@ namespace BoxRayTracer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BitmapRaytracer bmpTracer;
         public MainWindow()
         {
             InitializeComponent();
+            bmpTracer = new BitmapRaytracer(new Sphere(Vector.origin, 1), 20, (int)imageContainer.Width, (int)imageContainer.Height, 90, new Vector(-5, 0, 0), Vector.origin);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Bitmap bmp = bmpTracer.Render();
+            BitmapImage bmpImg = BitmapToImageSource(bmp);
+            imageContainer.Source = bmpImg;
+
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
         }
     }
 }
