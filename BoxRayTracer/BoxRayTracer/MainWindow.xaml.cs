9b298364f555e8f4c1e0eb538e41afe387e3ffc9
Cameron Richards <crichards1717@gmail.com>
2022-02-15 11:26:28 -0800

    Implemented settable object position, camera position, camera frustrum, and scaffolded additional render selections.

--- BoxRayTracer/BoxRayTracer/MainWindow.xaml.cs
+++ BoxRayTracer/BoxRayTracer/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Media.Imaging;
 using System.Drawing;
 using Scene;
 using Vector = Scene.Vector;
+using System;
 
 namespace BoxRayTracer
 {
@@ -13,16 +14,22 @@ namespace BoxRayTracer
         public MainWindow()
         {
             InitializeComponent();
-            bmpTracer = new BitmapRaytracer(new Sphere(Vector.origin, 1), 20, (int)imageContainer.Width, (int)imageContainer.Height, 90, new Vector(-5, 0, 0), Vector.origin);
-            SetParamFields();
+            SetDefaults();
+            bmpTracer = GetBRT();
         }
 
         private void Button_Render_Click(object sender, RoutedEventArgs e)
         {
+            bmpTracer = GetBRT();
             Bitmap bmp = bmpTracer.Render();
             BitmapImage bmpImg = BitmapToImageSource(bmp);
             imageContainer.Source = bmpImg;
             SetParamFields();
+        }
+
+        private void Button_Default_Click(object sender, RoutedEventArgs e)
+        {
+            SetDefaults();
         }
 
         private BitmapImage BitmapToImageSource(Bitmap bitmap)
@@ -43,13 +50,8 @@ namespace BoxRayTracer
 
         private void SetParamFields()
         {
-            // objPos is set in the constructor (currently static at origin)
-            objPosX.Text = $"{Vector.origin.x}";
-            objPosY.Text = $"{Vector.origin.y}";
-            objPosZ.Text = $"{Vector.origin.z}";
-
             bmpTracer.GetSceneParams(out Vector camPos, out Vector camFrus, out Vector camRoll);
-            
+
             camPosX.Text = $"{camPos.x}";
             camPosY.Text = $"{camPos.y}";
             camPosZ.Text = $"{camPos.z}";
@@ -61,6 +63,49 @@ namespace BoxRayTracer
             camRollX.Text = $"{camRoll.x}";
             camRollY.Text = $"{camRoll.y}";
             camRollZ.Text = $"{camRoll.z}";
+        }
+
+        private void SetDefaults()
+        {
+            objPosX.Text = Defaults.objPosX.ToString();
+            objPosY.Text = Defaults.objPosY.ToString();
+            objPosZ.Text = Defaults.objPosZ.ToString();
+
+            camPosX.Text = Defaults.camPosX.ToString();
+            camPosY.Text = Defaults.camPosY.ToString();
+            camPosZ.Text = Defaults.camPosZ.ToString();
+
+            camFrusX.Text = Defaults.camFrusX.ToString();
+            camFrusY.Text = Defaults.camFrusY.ToString();
+            camFrusZ.Text = Defaults.camFrusZ.ToString();
+
+            shapeSelector.ItemsSource = Defaults.shapeList;
+            shapeSelector.SelectedIndex = Defaults.shapeDefaultIndex;
+
+            camRollX.Text = "";
+            camRollY.Text = "";
+            camRollZ.Text = "";
+        }
+
+        private BitmapRaytracer GetBRT()
+        {
+            IDistanceEstimator dE;
+            switch (shapeSelector.SelectedItem) {
+                case "Sphere":
+                    dE = new Sphere(new Vector(Double.Parse(objPosX.Text), Double.Parse(objPosY.Text), Double.Parse(objPosZ.Text)), Defaults.radius);
+                    break;
+                // TODO: Default behavior?
+                default:
+                    dE = new Sphere(new Vector(Double.Parse(objPosX.Text), Double.Parse(objPosY.Text), Double.Parse(objPosZ.Text)), Defaults.radius);
+                    break;
+            }
+
+            // TODO: Handle invalid text entry
+            Vector camPos = new Vector(Double.Parse(camPosX.Text), Double.Parse(camPosY.Text), Double.Parse(camPosZ.Text));
+            Vector camFrus = new Vector(Double.Parse(camFrusX.Text), Double.Parse(camFrusY.Text), Double.Parse(camFrusZ.Text));
+
+            BitmapRaytracer brt = new BitmapRaytracer(dE, Defaults.maxDist, (int)imageContainer.Width, (int)imageContainer.Height, Defaults.fov, camPos, camFrus);
+            return brt;
         }
     }
 }
