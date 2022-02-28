62e80db73d18130944bf3749abc6c85f5ef42de4
Cameron Richards <crichards1717@gmail.com>
2022-02-28 14:43:30 -0800

    Refactored ISceneObjectEstimatable to abstract class to enforce Unit() return Vectors

---
+++ BoxRayTracer/Scene/DEs/SceneObjectEstimatable.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Scene
+{
+    public abstract class SceneObjectEstimatable
+    {
+        /// <summary>
+        /// The object's position in the Scene
+        /// </summary>
+        public Vector position { get; protected set; }
+        
+        /// <summary>
+        /// The object's material Color
+        /// </summary>
+        public Color color { get; protected set; }
+        
+        /// <summary>
+        /// Estimates the minimum distance to the object
+        /// </summary>
+        /// <param name="position">The point from which to estimate distance to the object</param>
+        /// <returns>Minimum distance, as double</returns>
+        public abstract double DE(Vector position);
+        
+        /// <summary>
+        /// The Normal vector at the given fragment position
+        /// </summary>
+        /// <param name="surfacePos">The fragment position at which to calculate the Normal</param>
+        /// <returns>Vector, normalized</returns>
+        public Vector Normal(Vector surfacePos)
+        {
+            return GetNormal(surfacePos).Unit();
+        }
+
+        protected abstract Vector GetNormal(Vector sufracePos);
+    }
+}
